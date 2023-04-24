132b6d98137896d70d898e593607a54425f5b245
nsca20 <130943163+nsca20@users.noreply.github.com>
2023-04-24 15:20:54 +0800

    updated script.js

--- script.js
+++ script.js
@@ -1,75 +1,36 @@
-// Variables to keep track of game state
-let currentPlayer = 'X';
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
-let gameActive = true;
-
-// DOM elements
-const cells = document.querySelectorAll('.cell');
-const statusMessage = document.querySelector('.status');
-const resetButton = document.querySelector('.reset');
+// Game state
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameover = false;
 
 // Function to handle cell click
-const handleCellClick = (event) => {
-  const clickedCell = event.target;
-  const clickedCellIndex = parseInt(clickedCell.dataset.index);
-
-  // Check if the cell is empty and the game is still active
-  if (gameBoard[clickedCellIndex] === '' && gameActive) {
-    gameBoard[clickedCellIndex] = currentPlayer;
-    clickedCell.textContent = currentPlayer;
-    clickedCell.classList.add(currentPlayer);
-
-    // Check for win or draw
-    if (checkWin() || checkDraw()) {
-      gameActive = false;
-      statusMessage.textContent = checkWin() ? `Player ${currentPlayer} wins!` : "It's a draw!";
-    } else {
-      // Switch to the other player
-      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-      statusMessage.textContent = `Player ${currentPlayer}'s turn`;
+function makeMove(index) {
+    if (!gameover && board[index] === "") {
+        board[index] = currentPlayer;
+        document.getElementById("board").children[index].innerText = currentPlayer;
+        currentPlayer = (currentPlayer === "X") ? "O" : "X";
+        checkWin();
     }
-  }
-};
+}
 
-// Function to check for a win
-const checkWin = () => {
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+// Function to check for win/draw
+function checkWin() {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-  for (let combination of winningCombinations) {
-    const [a, b, c] = combination;
-    if (gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-      return true;
+    for (let combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+            gameover = true;
+            alert("Player " + board[a] + " wins!");  
+        }
     }
-  }
-  return false;
-};
-
-// Function to check for a draw
-const checkDraw = () => {
-  return gameBoard.every(cell => cell !== '');
-};
-
-// Function to reset the game
-const resetGame = () => {
-  currentPlayer = 'X';
-  gameBoard = ['', '', '', '', '', '', '', '', ''];
-  gameActive = true;
-  statusMessage.textContent = `Player ${currentPlayer}'s turn`;
-  cells.forEach(cell => {
-    cell.textContent = '';
-    cell.classList.remove('X', 'O');
-  });
-};
-
-// Add click event listeners to cells and reset button
-cells.forEach(cell => cell.addEventListener('click', handleCellClick));
-resetButton.addEventListener('click', resetGame);
+}
