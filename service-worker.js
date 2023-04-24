4becc635b22a628c2898a8887cd65b68830fb95b
nsca20 <130943163+nsca20@users.noreply.github.com>
2023-04-24 14:08:38 +0800

    PWA recode

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,24 @@
-const CACHE_NAME = "tic-tac-toe-cache-v1";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/icons/icon-72x72.png",
-    "/icons/icon-96x96.png",
-    "/icons/icon-144x144.png",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-];
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
+      caches.open('tic-tac-toe-v1')
+        .then((cache) => {
+          return cache.addAll([
+            './index.html',
+            './style.css',
+            './script.js',
+            './icon-192x192.png',
+            './icon-512x512.png'
+          ]);
+        })
     );
-});
-
-self.addEventListener("fetch", (event) => {
+  });
+  
+  self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
+      caches.match(event.request)
+        .then((response) => {
+          return response || fetch(event.request);
+        })
     );
-});
+  });
+  
